636ffa6b48b06c8c4fd3e54c975e3d98816de802
jonathan777888 <kpakpojonathan37@gmail.com>
2026-06-17 23:18:40 +0000

    Add submit and exam result URL paths

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -5,11 +5,16 @@ app_name = 'onlinecourse'
 
 urlpatterns = [
     path('course/<int:course_id>/', views.course_details, name='course_details'),
-    path('course/<int:course_id>/submit/', views.submit, name='submit'),
+
+    path(
+        'course/<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
+
     path(
         'course/<int:course_id>/submission/<int:submission_id>/result/',
         views.show_exam_result,
         name='show_exam_result'
     ),
 ]
-path('', include('onlinecourse.urls')),
